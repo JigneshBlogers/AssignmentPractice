dd9a3f0950f00e4b1b7e0a8d4c84f60bbd5c1639
jigneshkalantri <jignesh.kalantri@wyndham.com>
2020-10-28 14:31:41 +0530

    Initial Commit For Repo, Setup completed

---
+++ AssignmentJignesh/AssignmentJignesh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentJignesh
+//
+//  Created by Kalantri, Jignesh (Cognizant) on 28/10/20.
+//  Copyright © 2020 Kalantri, Jignesh (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
